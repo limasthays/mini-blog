61efeaf84aee5e747686d8c5a7368641ec2ecbb0
Thays Lima <limasthays@gmail.com>
2022-09-22 16:34:36 -0300

    feature: rendering posts

--- src/components/posts-container/index.jsx
+++ src/components/posts-container/index.jsx
@@ -1,39 +1,61 @@
 import { PostsDiv } from './style'
+import { useEffect, useState } from 'react'
 import { PostWrapper } from '../post-wrapper'
 import { nanoid } from 'nanoid'
+import { api } from '../../api'
 
-const otherposts = [
-  {
-    name: 'Leanne Graham',
-    username: 'Bret',
-    userId: 1,
-    id: 1,
-    title:
-      'sunt aut facere repellat provident occaecati excepturi optio reprehenderit',
-    body: 'quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto',
-  },
-  {
-    name: 'Leanne Graham',
-    username: 'Bret',
-    userId: 1,
-    id: 2,
-    title: 'qui est esse',
-    body: 'est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla',
-  },
-]
+const posts = []
+const users = []
 
 export const PostsContainer = () => {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    api
+      .get('/posts')
+      .then((res) => {
+        posts.push(...res.data)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+
+    api
+      .get('/users')
+      .then((res) => {
+        users.push(...res.data)
+      })
+      .catch((err) => {
+        console.error(err)
+      })
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [])
+
   return (
     <PostsDiv>
-      {otherposts.map((item) => (
-        <PostWrapper
-          key={nanoid()}
-          name={item.name}
-          username={item.username}
-          title={item.title}
-          post={item.body}
-        />
-      ))}
+      {posts.map((post) => {
+        let filtered = users.find((user) => post.userId === user.id)
+        let { name, username } = filtered
+
+        return (
+          !loading && (
+            <PostWrapper
+              key={nanoid()}
+              name={name}
+              username={username}
+              title={post.title}
+              post={post.body}
+              postId={post.id}
+              onClick={() => console.log('deu bom!')}
+            />
+          )
+        )
+      })}
     </PostsDiv>
   )
 }
